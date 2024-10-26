3e3b3d968f7ac076519d5369ddb683901b21ccfc
brendan <brendangooch@gmail.com>
2024-10-26 10:50:41 +0100

    initial commit; wrote full implementation (v1.0.0) in single index.ts file

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # jest-expect
-provides wrapper functions for common jest expect methods for slightly different synatax
+Provides wrapper functions for common jest expect methods for slightly different syntax
+
+    import * as EXPECT from '@brendangooch/jest-expect';
+
+    test('test something', () => {
+        
+        expect(foo).toBe(10);
+        EXPECT.toBe(foo, 10);
+
+        expect(foo).not.toBe(20);
+        EXPECT.notToBe(foo, 20);
+
+        expect(foo).toBeTruthy();
+        EXPECT.truthy(foo);
+
+        // etc
+
+    })
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+
+export function toBe(expression: string | number, value: string | number): void {
+    expect(expression).toBe(value);
+}
+
+export function notToBe(expression: string | number, value: string | number): void {
+    expect(expression).not.toBe(value);
+}
+
+export function toBeCloseTo(expression: number, value: number, numDigits: number = 2): void {
+    expect(expression).toBeCloseTo(value);
+}
+
+export function truthy(expression: boolean): void {
+    expect(expression).toBeTruthy();
+}
+
+export function falsy(expression: boolean): void {
+    expect(expression).toBeFalsy();
+}
+
+export function toThrow(fn: Function): void {
+    expect(fn).toThrow();
+}
+
+export function toBeGreaterThan(expression: number, value: number): void {
+    expect(expression).toBeGreaterThan(value);
+}
+
+export function toBeGreaterThanOrEqual(expression: number, value: number): void {
+    expect(expression).toBeGreaterThanOrEqual(value);
+}
+
+export function toBeLessThan(expression: number, value: number): void {
+    expect(expression).toBeLessThan(value);
+}
+
+export function toBeLessThanOrEqual(expression: number, value: number): void {
+    expect(expression).toBeLessThanOrEqual(value);
+}
+
+
+export function toBeNull(expression: any): void {
+    expect(expression).toBeNull();
+}
+
+export function toBeUndefined(expression: any): void {
+    expect(expression).toBeUndefined();
+}
+
+export function toContain<t>(expression: Array<t>, value: any): void {
+    expect(expression).toContain(value);
+}
+
+export function toHaveLength<t>(expression: Array<t>, length: number): void {
+    expect(expression).toHaveLength(length);
+}
+
